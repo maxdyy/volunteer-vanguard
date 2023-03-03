a0594c1e96b3c620db8d9149bbf335aea2ff1b97
Keshav <50632203+KeshavDeepak@users.noreply.github.com>
2023-03-03 07:15:48 +0530

    Created the search bar functionality
    Data used for debugging is temporary

---
+++ js/database.js
@@ -0,0 +1,8 @@
+var database = [
+    {displayName : "Hawaii program"},
+    {displayName : "India program"},
+    {displayName : "Antarctica program"},
+    {displayName : "Indiana program"},
+    {displayName : "South America program"},
+    {displayName : "North America program"}
+]
